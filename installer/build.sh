0a12543f4e6f37b72d72b6fe97840f5a36481ce0
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 10:57:35 -0800

    fix macOS 64 bit code signing

--- installer/build.sh
+++ installer/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PACKAGE_NAME="SestoSenso_X2.pkg"
+BUNDLE_NAME="org.rti-zone.SestoSensoX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -f -s "$app_id_signature" --verbose ../build/Release/libSestoSenso.dylib
+fi
+
 mkdir -p ROOT/tmp/SestoSenso_X2/
 cp "../SestoSenso.ui" ROOT/tmp/SestoSenso_X2/
 cp "../SestoCalibrate.ui" ROOT/tmp/SestoSenso_X2/
@@ -7,12 +14,14 @@ cp "../PrimaLuceLab.png" ROOT/tmp/SestoSenso_X2/
 cp "../focuserlist PrimaLuceLab.txt" ROOT/tmp/SestoSenso_X2/
 cp "../build/Release/libSestoSenso.dylib" ROOT/tmp/SestoSenso_X2/
 
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.SestoSenso_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 SestoSenso_X2.pkg
-pkgutil --check-signature ./SestoSenso_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
+
 else
-pkgbuild --root ROOT --identifier org.rti-zone.SestoSenso_X2 --scripts Scripts --version 1.0 SestoSenso_X2.pkg
+    pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
